d7396a47138703595028c0fb3e0902d22f143847
DeepakGathwal <rickygathwal@gmail.com>
2022-07-23 23:43:23 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'linkedinAuth': {
+      'clientID': '77fnteaheywgg1', // your App ID
+      'clientSecret': 'nDsMqSxErJBa2SIn', // your App Secret
+      'callbackURL': 'http://127.0.0.1:3000/auth/linkedin/callback'
+    }
+  }
